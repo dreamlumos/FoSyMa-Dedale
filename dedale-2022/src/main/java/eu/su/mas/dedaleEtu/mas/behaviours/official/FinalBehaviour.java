218ff952740fea13c37ecb152a89f260100feb67
3702742 <zlouised@gmail.com>
2022-05-09 00:31:52 +0200

    hell map

--- dedale-2022/src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/official/FinalBehaviour.java
+++ dedale-2022/src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/official/FinalBehaviour.java
@@ -5,6 +5,8 @@ import eu.su.mas.dedaleEtu.mas.agents.official.ExploreDFSAgent;
 import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
 
+import java.util.Objects;
+
 public class FinalBehaviour extends OneShotBehaviour {
 
 	private static final long serialVersionUID = 641341260534295805L;
@@ -20,10 +22,10 @@ public class FinalBehaviour extends OneShotBehaviour {
 		String type = myAgent.getType();
 		
 		System.out.println("[FinalBehaviour] Type of "+myAgent.getLocalName()+": "+type);
-		if (type == "Gold") {
+		if (Objects.equals(type, "Gold")) {
 			System.out.println("[FinalBehaviour] "+myAgent.getLocalName()+"'s original capacity: "+ec.getGoldCapacity());
 		}
-		if (type == "Diamond") {
+		if (Objects.equals(type, "Diamond")) {
 			System.out.println("[FinalBehaviour] "+myAgent.getLocalName()+"'s original capacity: "+ec.getDiamondCapacity());
 		}
 		System.out.println("[FinalBehaviour] "+myAgent.getLocalName() + " remaining capacity: " + myAgent.getBackPackFreeSpace());
