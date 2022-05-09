28d8e67fb14e12af517efa57eb5ce13666d34089
dreamlumos <30956230+dreamlumos@users.noreply.github.com>
2022-05-09 08:01:50 +0200

    fixed edge merging which i think i broke yday

--- dedale-2022/src/main/java/eu/su/mas/dedaleEtu/mas/knowledge/FullMapRepresentation.java
+++ dedale-2022/src/main/java/eu/su/mas/dedaleEtu/mas/knowledge/FullMapRepresentation.java
@@ -1,6 +1,5 @@
 package eu.su.mas.dedaleEtu.mas.knowledge;
 
-import java.awt.SystemTray;
 import java.io.Serializable;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -92,22 +91,16 @@ public class FullMapRepresentation implements Serializable {
 
 			n = this.g.addNode(nodeId);
 			newNode = true;
-			
-//			if (lastVisitTimestamp != -1) { // Special case: the node the agent starts on will be immediately closed
-//				n.setAttribute("ui.class", NodeStatus.closed.toString());
-//			} else {
-//				n.setAttribute("ui.class", NodeStatus.open.toString());
-//			}
-//			
-//			n.setAttribute("ui.label", nodeId);
-//			n.setAttribute("timestamp", lastVisitTimestamp);
+			// Keep in mind the following special case: the node the agent starts on will be immediately closed
+
 		} else { // Updating known node
 			n = this.g.getNode(nodeId);
 		}
+		
 		Object previousTimestamp = n.getAttribute("timestamp");
 		Object previousStenchTimestamp = n.getAttribute("stenchTimestamp");
 
-		if (previousTimestamp == null || lastVisitTimestamp > (long) previousTimestamp) {
+		if (previousTimestamp == null || lastVisitTimestamp > (long) previousTimestamp) { // I have never visited this node, or if I have, the passed information is more recent
 			n.clearAttributes();
 			n.setAttribute("ui.label", nodeId);
 			n.setAttribute("timestamp", lastVisitTimestamp);
@@ -117,6 +110,7 @@ public class FullMapRepresentation implements Serializable {
 				n.setAttribute("ui.class", NodeStatus.open.toString());
 			}
 		}
+		
 		for (Couple<Observation, Integer> o: lObservations){
 			Observation observationType = o.getLeft();
 			Integer observationValue = o.getRight();
@@ -177,55 +171,6 @@ public class FullMapRepresentation implements Serializable {
 	}
 
 	/**
-	 * Compute the Map to send from ag1's map and a list of nodes.
-	 *
-	 * @param nodesToShare list of id of the nodes to be shared to ag2
-	 * @return part of ag1's Map to be sent to ag2 to update ag2's Map
-	 */
-	public FullMapRepresentation getPartialMap(ArrayList<String> nodesToShare) {
-		FullMapRepresentation partialMap = new FullMapRepresentation(false);
-		if (nodesToShare == null) {
-			return partialMap;
-		}
-		ArrayList<String> nodesToBeSent = new ArrayList<String>();
-		for (String nodeId: nodesToShare) {
-			Node oldNode = this.g.getNode(nodeId);
-			Node newNode = partialMap.g.addNode(nodeId);
-			nodesToBeSent.add(nodeId);
-			for (Object attribute : oldNode.attributeKeys().toArray()) {
-				newNode.setAttribute((String) attribute, oldNode.getAttribute((String) attribute));
-			}
-		}
-		for (String nodeId: nodesToShare) {
-			Node n = this.g.getNode(nodeId);
-			for (Object edge: n.edges().toArray()) {
-//				String edgeId = ((Edge) edge).getId();
-//				Edge oldEdge = this.g.getEdge(edgeId);
-				String node0 = ((Edge) edge).getNode0().getId();
-				String node1 = ((Edge) edge).getNode1().getId();
-				if (!nodesToBeSent.contains(node0)) {
-					Node oldNode = this.g.getNode(node0);
-					Node newNode = partialMap.g.addNode(node0);
-					nodesToBeSent.add(node0);
-					for (Object attribute : oldNode.attributeKeys().toArray()) {
-						newNode.setAttribute((String) attribute, oldNode.getAttribute((String) attribute));
-					}
-				}
-				if (!nodesToBeSent.contains(node1)) {
-					Node oldNode = this.g.getNode(node1);
-					Node newNode = partialMap.g.addNode(node1);
-					nodesToBeSent.add(node1);
-					for (Object attribute : oldNode.attributeKeys().toArray()) {
-						newNode.setAttribute((String) attribute, oldNode.getAttribute((String) attribute));
-					}
-				}
-				partialMap.addEdge(node0, node1);
-			}
-		}
-		return partialMap;
-	}
-
-	/**
 	 * Compute the shortest Path from idFrom to IdTo. The computation is currently not very efficient
 	 * 
 	 * @param idFrom id of the origin node
@@ -235,20 +180,20 @@ public class FullMapRepresentation implements Serializable {
 	public synchronized List<String> getShortestPath(String idFrom, String idTo) {
 		List<String> shortestPath = new ArrayList<String>();
 
-		Dijkstra dijkstra = new Dijkstra();//number of edge
+		Dijkstra dijkstra = new Dijkstra(); //number of edge
 		dijkstra.init(g);
 		dijkstra.setSource(g.getNode(idFrom));
-		dijkstra.compute();//compute the distance to all nodes from idFrom
-		List<Node> path=dijkstra.getPath(g.getNode(idTo)).getNodePath(); //the shortest path from idFrom to idTo
-		Iterator<Node> iter=path.iterator();
+		dijkstra.compute(); //compute the distance to all nodes from idFrom
+		List<Node> path = dijkstra.getPath(g.getNode(idTo)).getNodePath(); //the shortest path from idFrom to idTo
+		Iterator<Node> iter = path.iterator();
 		while (iter.hasNext()) {
 			shortestPath.add(iter.next().getId());
 		}
 		dijkstra.clear();
-		if (shortestPath.isEmpty()) {//The openNode is not currently reachable
+		if (shortestPath.isEmpty()) { //The openNode is not currently reachable
 			return null;
 		} else {
-			shortestPath.remove(0);//remove the current position
+			shortestPath.remove(0); //remove the current position
 		}
 		return shortestPath;
 	}
@@ -330,15 +275,158 @@ public class FullMapRepresentation implements Serializable {
 				.collect(Collectors.toList());
 	}
 
-	/**
-	 * Before the migration we kill all non serializable components and store their data in a serializable form
-	 */
-	public void prepareMigration(){
-		serializeGraphTopology();
-		closeGui();
-		this.g = null;
+	/* Only takes into account information that is more recent. */
+	public void mergeNode(SerializableNode<String, HashMap<String, Object>> n2) {
+		
+		// n1 : our node
+		// n2 : other agent's node
+		
+		String id = n2.getNodeId();
+		HashMap<String, Object> n2Attributes = n2.getNodeContent();
+		
+		Node n1 = this.g.getNode(id);
+		if (n1 == null) {
+			n1 = this.g.addNode(id);
+			//System.out.println("[FullMapRep::mergeNode] Adding node "+id+" to receiving agent's map.");
+		} else {
+			//System.out.println("[FullMapRep::mergeNode] Node "+id+" is already in the receiving agent's map.");
+		}
+		
+		Object n1Timestamp = n1.getAttribute("timestamp");
+		Object n1StenchTimestamp = n1.getAttribute("stenchTimestamp");
+		Object n1Class = n1.getAttribute("ui.class");
+		Object n2Timestamp = n2Attributes.get("timestamp");
+		Object n2StenchTimestamp = n2Attributes.get("stenchTimestamp");
+		Object n2Class = n2Attributes.get("ui.class");
+
+		if (n2Timestamp != null && (n1Timestamp == null || (long) n2Timestamp > (long) n1Timestamp)) {
+			n1.clearAttributes();
+		}
+		
+		for (String att: n2Attributes.keySet()) {
+			
+			if (att.equals("Stench") && n2StenchTimestamp != null && (n1StenchTimestamp == null || (long) n2StenchTimestamp > (long) n1StenchTimestamp)) {
+				n1.setAttribute("Stench", n2Attributes.get("Stench"));
+				n1.setAttribute("stenchTimestamp", n2StenchTimestamp);
+			} else if (att.equals("ui.class")){
+				if (NodeStatus.closed.toString().equals(n1Class) || NodeStatus.closed.toString().equals(n2Class)) {
+					n1.setAttribute(att, NodeStatus.closed.toString());
+					//System.out.println("[FullMapRep::mergeNode] Node "+id+" is closed.");
+				} else {
+					n1.setAttribute(att, NodeStatus.open.toString());
+				}
+			} else if (att.equals("Gold")) { 
+				if (n2Timestamp != null && (n1Timestamp == null || (long) n2Timestamp > (long) n1Timestamp)) {
+					n1.setAttribute(att, n2Attributes.get(att));
+				}
+				this.goldDict.put(id, (Integer) n2Attributes.get(att));
+			} else if (att.equals("Diamond")) {
+				if (n2Timestamp != null && (n1Timestamp == null || (long) n2Timestamp > (long) n1Timestamp)) {
+					n1.setAttribute(att, n2Attributes.get(att));
+				}
+				this.diamondDict.put(id, (Integer) n2Attributes.get(att));
+			} else if (!att.equals("stenchTimestamp")) { // att is one of [ui.label, LockIsOpen, timestamp]
+				if (n2Timestamp != null && (n1Timestamp == null || (long) n2Timestamp > (long) n1Timestamp)) {
+					n1.setAttribute(att, n2Attributes.get(att));
+				}
+			}
+		}	
+	}
+	
+	public void mergeMap(SerializableSimpleGraph<String, HashMap<String, Object>> sgreceived) {
+
+		for (SerializableNode<String, HashMap<String, Object>> serializableNode0: sgreceived.getAllNodes()){
+			
+			// Merging node
+			mergeNode(serializableNode0);
+			String node0Id = serializableNode0.getNodeId();
+			
+			// Merging edges
+			for (String node1Id: sgreceived.getEdges(node0Id)){ // getEdges returns list of nodes that are connected to node0
+				addEdge(node0Id, node1Id);
+			}
+		}
+		//System.out.println("Merge done");
 	}
 
+	/**
+	 * 
+	 * @return true if there exists at least one openNode on the graph 
+	 */
+	public boolean hasOpenNode() {
+		return (this.g.nodes()
+				.filter(n -> n.getAttribute("ui.class") == NodeStatus.open.toString())
+				.findAny()).isPresent();
+	}
+
+	public HashMap<String, Integer> getDiamondDict(){
+		return diamondDict;
+	}
+	
+	public HashMap<String, Integer> getGoldDict(){
+		return goldDict;
+	}
+
+	public void setDiamondDict(HashMap<String, Integer> diamondDict){
+		this.diamondDict = diamondDict;
+	}
+
+	public void setGoldDict(HashMap<String, Integer> goldDict){
+		this.goldDict = goldDict;
+	}
+	
+
+	/**
+	 * Compute the Map to send from ag1's map and a list of nodes.
+	 *
+	 * @param nodesToShare list of id of the nodes to be shared to ag2
+	 * @return part of ag1's Map to be sent to ag2 to update ag2's Map
+	 */
+	public FullMapRepresentation getPartialMap(ArrayList<String> nodesToShare) {
+		FullMapRepresentation partialMap = new FullMapRepresentation(false);
+		if (nodesToShare == null) {
+			return partialMap;
+		}
+		ArrayList<String> nodesToBeSent = new ArrayList<String>();
+		// Adding the nodes
+		for (String nodeId: nodesToShare) {
+			Node oldNode = this.g.getNode(nodeId);
+			Node newNode = partialMap.g.addNode(nodeId);
+			nodesToBeSent.add(nodeId);
+			for (Object attribute : oldNode.attributeKeys().toArray()) {
+				newNode.setAttribute((String) attribute, oldNode.getAttribute((String) attribute));
+			}
+		}
+		// Adding the edges
+		for (String nodeId: nodesToShare) {
+			Node n = this.g.getNode(nodeId);
+			for (Object edge: n.edges().toArray()) {
+				String node0 = ((Edge) edge).getNode0().getId();
+				String node1 = ((Edge) edge).getNode1().getId();
+				if (!nodesToBeSent.contains(node0)) {
+					Node oldNode = this.g.getNode(node0);
+					Node newNode = partialMap.g.addNode(node0);
+					nodesToBeSent.add(node0);
+					for (Object attribute : oldNode.attributeKeys().toArray()) {
+						newNode.setAttribute((String) attribute, oldNode.getAttribute((String) attribute));
+					}
+				}
+				if (!nodesToBeSent.contains(node1)) {
+					Node oldNode = this.g.getNode(node1);
+					Node newNode = partialMap.g.addNode(node1);
+					nodesToBeSent.add(node1);
+					for (Object attribute : oldNode.attributeKeys().toArray()) {
+						newNode.setAttribute((String) attribute, oldNode.getAttribute((String) attribute));
+					}
+				}
+				partialMap.addEdge(node0, node1);
+			}
+		}
+		return partialMap;
+	}
+
+	/* ---------------- SERIALIZATION METHODS ---------------- */
+	
 	/**
 	 * Before sending the agent knowledge of the map it should be serialized.
 	 */
@@ -372,7 +460,18 @@ public class FullMapRepresentation implements Serializable {
 		serializeGraphTopology();
 		return this.sg;
 	}
-
+	
+	/* ---------------- MIGRATION METHODS ---------------- */
+	
+	/**
+	 * Before the migration we kill all non serializable components and store their data in a serializable form.
+	 */
+	public void prepareMigration(){
+		serializeGraphTopology();
+		closeGui();
+		this.g = null;
+	}
+	
 	/**
 	 * After migration we load the serialized data and recreate the non serializable components (Gui,..)
 	 */
@@ -423,107 +522,4 @@ public class FullMapRepresentation implements Serializable {
 		g.display();
 	}
 	
-	/* takes most recent nodes*/
-	public void mergeNode(SerializableNode<String, HashMap<String, Object>> n2) {
-		
-		// n1 : our node
-		// n2 : other agent's node
-		
-		String id = n2.getNodeId();
-		HashMap<String, Object> n2Attributes = n2.getNodeContent();
-		
-		Node n1 = this.g.getNode(id);
-		if (n1 == null) {
-			n1 = this.g.addNode(id);
-			System.out.println("[FullMapRep::mergeNode] Adding node "+id+" to receiving agent's map.");
-		} else {
-			System.out.println("[FullMapRep::mergeNode] Node "+id+" is already in the receiving agent's map.");
-		}
-		
-		Object n1Timestamp = n1.getAttribute("timestamp");
-		Object n1StenchTimestamp = n1.getAttribute("stenchTimestamp");
-		Object n1Class = n1.getAttribute("ui.class");
-		Object n2Timestamp = n2Attributes.get("timestamp");
-		Object n2StenchTimestamp = n2Attributes.get("stenchTimestamp");
-		Object n2Class = n2Attributes.get("ui.class");
-
-		if (n2Timestamp != null && (n1Timestamp == null || (long) n2Timestamp > (long) n1Timestamp)) {
-			n1.clearAttributes();
-		}
-		
-		for (String att: n2Attributes.keySet()) {
-			
-			if (att.equals("Stench") && n2StenchTimestamp != null && (n1StenchTimestamp == null || (long) n2StenchTimestamp > (long) n1StenchTimestamp)) {
-				n1.setAttribute("Stench", n2Attributes.get("Stench"));
-				n1.setAttribute("stenchTimestamp", n2StenchTimestamp);
-			} else if (att.equals("ui.class")){
-				if (NodeStatus.closed.toString().equals(n1Class) || NodeStatus.closed.toString().equals(n2Class)) {
-					n1.setAttribute(att, NodeStatus.closed.toString());
-					System.out.println("[FullMapRep::mergeNode] Node "+id+" is closed.");
-				} else {
-					n1.setAttribute(att, NodeStatus.open.toString());
-				}
-			} else if (att.equals("Gold")) { 
-				if (n2Timestamp != null && (n1Timestamp == null || (long) n2Timestamp > (long) n1Timestamp)) {
-					n1.setAttribute(att, n2Attributes.get(att));
-				}
-				this.goldDict.put(id, (Integer) n2Attributes.get(att));
-			} else if (att.equals("Diamond")) {
-				if (n2Timestamp != null && (n1Timestamp == null || (long) n2Timestamp > (long) n1Timestamp)) {
-					n1.setAttribute(att, n2Attributes.get(att));
-				}
-				this.diamondDict.put(id, (Integer) n2Attributes.get(att));
-			} else if (!att.equals("stenchTimestamp")) { // att is one of [ui.label, LockIsOpen, timestamp]
-				if (n2Timestamp != null && (n1Timestamp == null || (long) n2Timestamp > (long) n1Timestamp)) {
-					n1.setAttribute(att, n2Attributes.get(att));
-				}
-			}
-		}	
-	}
-	
-	public void mergeMap(SerializableSimpleGraph<String, HashMap<String, Object>> sgreceived) {
-
-		// Nodes
-		for (SerializableNode<String, HashMap<String, Object>> n: sgreceived.getAllNodes()){
-			mergeNode(n);
-		}
-
-		// Edges
-		for (SerializableNode<String, HashMap<String, Object>> n: sgreceived.getAllNodes()){
-			for (String s: sgreceived.getEdges(n.getNodeId())){
-				if (this.g.getEdge(s) == null) {
-					addEdge(n.getNodeId(), s);
-				}
-			}
-		}
-		
-		//System.out.println("Merge done");
-	}
-
-	/**
-	 * 
-	 * @return true if there exist at least one openNode on the graph 
-	 */
-	public boolean hasOpenNode() {
-		return (this.g.nodes()
-				.filter(n -> n.getAttribute("ui.class") == NodeStatus.open.toString())
-				.findAny()).isPresent();
-	}
-
-	public HashMap<String, Integer> getDiamondDict(){
-		return diamondDict;
-	}
-	
-	public HashMap<String, Integer> getGoldDict(){
-		return goldDict;
-	}
-
-	public void setDiamondDict(HashMap<String, Integer> diamondDict){
-		this.diamondDict = diamondDict;
-	}
-
-	public void setGoldDict(HashMap<String, Integer> goldDict){
-		this.goldDict = goldDict;
-	}
-
 }
